3432159f237c218eb7699aef072710e423e97e28
ConorRoberts <62777912+ConorRoberts@users.noreply.github.com>
2020-10-20 13:24:34 -0400

    Updates

---
+++ precache-manifest.937dccc13021c1021e3e5ade3defdb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd075edb3249ac4f95519585b0bc3a1e",
+    "url": "/campus-connect/index.html"
+  },
+  {
+    "revision": "75e26d9e182acf907e02",
+    "url": "/campus-connect/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db236bf5d457edb5bb89",
+    "url": "/campus-connect/static/js/2.ad505e15.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/campus-connect/static/js/2.ad505e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e26d9e182acf907e02",
+    "url": "/campus-connect/static/js/main.d38eb681.chunk.js"
+  },
+  {
+    "revision": "690481a2612596018938",
+    "url": "/campus-connect/static/js/runtime-main.007b86de.js"
+  }
+]);
